e63885ab83c6ee9e9a3024fd0d768a44bb1d6e30
David Martínez Martí <deavidsedice@gmail.com>
2008-08-27 20:04:02 +0200

    Corrected problems with the smooth camera

--- tests/traveling-camera-1.py
+++ tests/traveling-camera-1.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python 
-	
 import worlds
+from worlds import soya 
 
 worlds.init()
 
 head = worlds.Body("caterpillar_head")
-
-head.rotation[1]=5.0
-head.velocity.z=-0.1
 
 new_springfactor = springfactor = 0
 
@@ -17,41 +14,41 @@ for i in range(100):
 	pieces[i] = worlds.FollowBody("caterpillar",previous)
 	previous = pieces[i]
 
-traveling = worlds.soya.ThirdPersonTraveling(pieces[0])
+worlds.camera = soya.TravelingCamera(worlds.scene)
+
+traveling = soya.ThirdPersonTraveling(pieces[5])
 traveling.distance = 35.0
 
 worlds.camera.add_traveling(traveling)
-worlds.camera.speed = 1
-
-worlds.camera.set_xyz(0,35,25)
-# worlds.camera.look_at(head)
-
+worlds.camera.speed = 0.05
 
 def mainloop():
 	global head, new_springfactor, springfactor
-	if worlds.soya.sdlconst.K_UP in worlds.KEY: 		
-		head.velocity.z=-1;
+	if soya.sdlconst.K_UP in worlds.KEY: 		
+		head.velocity.z=-1
 	else:
 		head.velocity.z/=1.1
-	if worlds.soya.sdlconst.K_DOWN in worlds.KEY:	
-		head.velocity.z+=0.05; 
-	if worlds.soya.sdlconst.K_LEFT in worlds.KEY:	head.rotation[1]+=1
-	if worlds.soya.sdlconst.K_RIGHT in worlds.KEY:	head.rotation[1]-=1
+	if soya.sdlconst.K_DOWN in worlds.KEY:	
+		head.velocity.z=0.5
+	if soya.sdlconst.K_LEFT in worlds.KEY:	head.rotation[1]+=1
+	if soya.sdlconst.K_RIGHT in worlds.KEY:	head.rotation[1]-=1
+	
 	head.rotation[1]/=1.05
 
-	if worlds.soya.sdlconst.K_1 in worlds.KEY:	new_springfactor=1
-	if worlds.soya.sdlconst.K_2 in worlds.KEY:	new_springfactor=10
-	if worlds.soya.sdlconst.K_3 in worlds.KEY:	new_springfactor=25
-	if worlds.soya.sdlconst.K_4 in worlds.KEY:	new_springfactor=50
-	if worlds.soya.sdlconst.K_5 in worlds.KEY:	new_springfactor=75
-	if worlds.soya.sdlconst.K_0 in worlds.KEY:	new_springfactor=0
+	if soya.sdlconst.K_1 in worlds.KEY:	new_springfactor=0
+	if soya.sdlconst.K_2 in worlds.KEY:	new_springfactor=50
+	if soya.sdlconst.K_3 in worlds.KEY:	new_springfactor=65
+	if soya.sdlconst.K_4 in worlds.KEY:	new_springfactor=75
+	if soya.sdlconst.K_5 in worlds.KEY:	new_springfactor=80
+	if soya.sdlconst.K_6 in worlds.KEY:	new_springfactor=85
+	if soya.sdlconst.K_7 in worlds.KEY:	new_springfactor=90
+	if soya.sdlconst.K_8 in worlds.KEY:	new_springfactor=95
 
 	if new_springfactor != springfactor:
 		global pieces
 		for i in pieces:
 			pieces[i].set_springfactor(new_springfactor)
-		
-		new_springfactor = springfactor
+		springfactor =	new_springfactor 
 		
 def renderloop(proportion):
 	pass
