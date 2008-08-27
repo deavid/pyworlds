36f1b2165d35bb2fab78c58633740018b749ca9b
David Martínez Martí <deavidsedice@gmail.com>
2008-08-27 14:10:26 +0200

    Better performance of FollowBody

--- tests/basic-5.py
+++ tests/basic-5.py
@@ -12,23 +12,28 @@ head.velocity.z=-0.1
 
 piece = {}
 previous = head
-for i in range(10):
+for i in range(50):
 	piece[i] = worlds.FollowBody("caterpillar",previous)
 	previous = piece[i]
 
-worlds.camera.set_xyz(0,15,15)
+worlds.camera.set_xyz(0,25,15)
 worlds.camera.look_at(head)
 
 def mainloop():
 	global head
-	if soya.sdlconst.K_UP in worlds.KEY: 		head.velocity.z-=0.01
-	if soya.sdlconst.K_DOWN in worlds.KEY:	head.velocity.z+=0.01
+	if soya.sdlconst.K_UP in worlds.KEY: 		
+		head.velocity.z-=0.01; head.velocity.z*=1.05
+	else:
+		head.velocity.z/=1.1
+	if soya.sdlconst.K_DOWN in worlds.KEY:	
+		head.velocity.z+=0.01; 
 	if soya.sdlconst.K_LEFT in worlds.KEY:	head.rotation[1]+=1
 	if soya.sdlconst.K_RIGHT in worlds.KEY:	head.rotation[1]-=1
 	head.rotation[1]/=1.1
 		
 		
 def renderloop(proportion):
+	pass
 	# worlds.camera.look_at(head) # This looks buggy
 
 
