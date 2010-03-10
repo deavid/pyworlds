5891816214ca181db78a2ee8bf53796f243c4d42
David Martinez Marti <desarrollo@gestiweb.com>
2010-03-10 08:40:51 +0100

    Performance changes

--- src/pyworlds/basics/scene.py
+++ src/pyworlds/basics/scene.py
@@ -4,18 +4,21 @@ import pyworlds.basics.body
 class Scene(soya.World,pyworlds.basics.body.Body):
 
 	round_duration=.04
-	def begin_round(self):
+        # Commented because it does nothing and it uses a 6% of CPU
+	"""def begin_round(self):
 		soya.World.begin_round(self)
 		# TODO: place master callback here!
-
-	def advance_time(self, proportion):
+        """
+        # Commented because it does nothing and it uses a 6% of CPU
+	"""def advance_time(self, proportion):
 		soya.World.advance_time(self, proportion)
 		# TODO: place master callback here!
-
-	def end_round(self):
+        """
+        # Commented because it does nothing and it uses a 6% of CPU
+	"""def end_round(self):
 		soya.World.end_round(self)
 		# TODO: place master callback here!
-
+        """
 scene = None
 
 
